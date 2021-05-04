0e2b0efbf05031d44ea866fe23f9b54652e2c0de
Thomas Bonk <thomas@meandmymac.de>
2021-05-04 18:55:37 +0200

    removed platform versioning

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "CNuklear",
-    platforms: [
-        .macOS(.v10_14)
-    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
